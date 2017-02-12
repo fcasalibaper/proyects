fe4426519872ed187ae5facd316acefa61c6e8e9
Fernando Casaliba Perlangeli <fernando.casaliba@gmail.com>
2017-02-12 05:03:43 -0300

    kattieann

---
+++ html/lookbook.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<?php include 'includes/head.inc.php'; ?>
+</head>
+
+<body>
+	<div class="cfull">
+		<div class="grid">
+			<div class="grid-wrap">
+				<div class="grid-line"></div>
+				<div class="grid-line"></div>
+				<div class="grid-line"></div>
+				<div class="grid-line"></div>
+				<div class="grid-line"></div>
+			</div>
+		</div>
+
+		<div class="container-fluid">
+			<div class="row modulo">
+				<!-- header -->
+				<?php include 'includes/header.inc.php'; ?>
+
+				<div class="lookbook">
+
+					<hgroup>
+						<h1 class="title">
+							Lookbook
+						</h1>
+					</hgroup>
+				
+					<div class="lookbook__col lookbook__col1">
+						<ul>
+							<li><strong>DESIGNER</strong> Katie McGuigan</li>
+							<li><strong>PHOTOGRAPHY</strong> Caoimhe Hahn</li>
+							<li><strong>HAIR</strong> Danilo</li>
+							<li><strong>MAKE UP</strong> Ibi Molnar</li>
+							<li><strong>MODEL</strong> Sophie Allen</li>
+						</ul>
+					</div>
+					<div class="lookbook__col lookbook__col2">
+						<img src="https://s-media-cache-ak0.pinimg.com/736x/76/46/bc/7646bc218ff6b3c2dbed3f57753b7f7a.jpg" alt="" />
+						<img src="http://static1.squarespace.com/static/53ab3844e4b0c8440b30cec4/t/54bfecece4b068bb534e5786/1421864173710/?format=1000w" alt="" />
+						<img src="http://basicmodels.com.sg/sites/default/files/styles/original_optimized/public/test_with_faye_hur_6.jpg?itok=0oGfyQbl" alt="" />
+					</div>
+
+					<div class="lookbook__slider swiper-container">						
+				    <div class="swiper-wrapper">
+			        <!-- Slides -->
+			        <div class="swiper-slide"><img src="https://s-media-cache-ak0.pinimg.com/736x/e0/7d/86/e07d86b156ff07dabe38806e0a03c80f.jpg"></div>
+			        <div class="swiper-slide"><img src="https://s-media-cache-ak0.pinimg.com/originals/2d/c9/3c/2dc93cdd3bed89f035d5dfb89e1d3be3.jpg"></div>
+			        <div class="swiper-slide"><img src="http://www.newyorkdress.com/Images/CMS/museum1.jpg"></div>
+			        <div class="swiper-slide"><img src="http://farm8.staticflickr.com/7141/6387116305_bf33e07877_o.jpg"></div>
+			        <div class="swiper-slide"><img src="http://seoulbeats.com/wp-content/uploads/2012/08/20120801_seoulbeats_sistar_bora.jpg"></div>
+			        <div class="swiper-slide"><img src="https://s-media-cache-ak0.pinimg.com/736x/68/0f/71/680f71082f80db079c461527de17a919.jpg"></div>			        
+			        <div class="swiper-slide"><img src="https://s-media-cache-ak0.pinimg.com/736x/ba/61/f7/ba61f701e3ded64da45bb5c4a19efeb7.jpg"></div>
+			        <div class="swiper-slide"><img src="https://s-media-cache-ak0.pinimg.com/736x/e0/7d/86/e07d86b156ff07dabe38806e0a03c80f.jpg"></div>
+			        <div class="swiper-slide"><img src="https://s-media-cache-ak0.pinimg.com/originals/2d/c9/3c/2dc93cdd3bed89f035d5dfb89e1d3be3.jpg"></div>
+			        <div class="swiper-slide"><img src="https://s-media-cache-ak0.pinimg.com/736x/68/0f/71/680f71082f80db079c461527de17a919.jpg"></div>
+			        <div class="swiper-slide"><img src="https://s-media-cache-ak0.pinimg.com/736x/ba/61/f7/ba61f701e3ded64da45bb5c4a19efeb7.jpg"></div>
+			        <div class="swiper-slide"><img src="http://www.newyorkdress.com/Images/CMS/museum1.jpg"></div>
+			        <div class="swiper-slide"><img src="http://farm8.staticflickr.com/7141/6387116305_bf33e07877_o.jpg"></div>
+			        <div class="swiper-slide"><img src="http://seoulbeats.com/wp-content/uploads/2012/08/20120801_seoulbeats_sistar_bora.jpg"></div>
+				    </div>	
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- scripts -->
+  <?php include 'includes/js.inc.php'; ?>	
+</body>
+</html>
