d60a9306f723ef1e68aaadc7e8b12c5230642553
Fernando Casaliba Perlangeli <fernando.casaliba@gmail.com>
2017-02-20 21:09:39 -0300

    intento - galeria interna
    probar en Mac (que no aparezca scroll):
    http://localhost/katieann/html/aw2017_flex.php
    http://localhost/katieann/html/aw2017_absolute.php

---
+++ kattieAnn/html/aw2017_flex.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<?php include 'includes/head.inc.php'; ?>
+</head>
+
+<body>
+	<div class="cfull">
+		<?php include 'includes/grid.inc.php'; ?>
+
+		<div class="container-fluid">
+			<div class="row modulo">								
+
+				<!-- slider -->
+				<div class="section interna">
+
+					<div class="col1">						
+						<hgroup>
+							<h1 class="title">
+								AW 17
+							</h1>
+						</hgroup>
+
+						<!-- back button -->
+						<aside class="back__button">
+							<a href="index.php">
+								back home
+								<div class="back__icon">
+									<i class="icon-left"></i>
+								</div>
+							</a>
+						</aside>
+					</div>
+
+					<!-- slider -->
+					<div class="interna__slider interna__slider swiper-container">						
+				    <div class="swiper-wrapper">
+			        <!-- Slides -->			        			        
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-1.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-2.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-3.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-4.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-5.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-6.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-7.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-8.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-9.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-10.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-11.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-12.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-13.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-14.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-15.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-16.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-17.jpg" alt="" />
+			        	</figure>
+			        </div>
+			        <div class="swiper-slide swiper-slide-figure">
+			        	<figure>
+			        		<img src="images/aw/aw-18.jpg" alt="" />
+			        	</figure>
+			        </div>
+				    </div>	
+					</div>					
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<!-- scripts -->
+  <?php include 'includes/js.inc.php'; ?>	
+</body>
+</html>
