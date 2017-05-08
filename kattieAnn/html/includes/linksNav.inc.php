d160237afd4d26cc0097957c57c019907683ef7d
Fernando Casaliba Perlangeli <fernando.casaliba@gmail.com>
2017-05-08 10:50:20 -0300

    link de presnsa

--- kattieAnn/html/includes/linksNav.inc.php
+++ kattieAnn/html/includes/linksNav.inc.php
@@ -1,3 +1,3 @@
 <li><a class="btn-link-white" href="http://fashionscout.co.uk/katie-ann-mcguigan" target="_blank">http://fashionscout.co.uk/katie-ann-mcguigan/</a></li>
 <li><a class="btn-link-white" href="mailto:info@katiemcguigan.com">info@katiemcguigan.com</a></li>
-<li>Press Contact: <a class="btn-link-white" href="mailto:sidney@agencyeleven.co.uk">sidney@agencyeleven.co.uk</a></li>
+<li>Press Contact: <a class="btn-link-white" href="mailto:alex@thedecemberagency.com">alex@thedecemberagency.com</a></li>
